e0286222fc717caae4e7aa69e7fea9a4a0c4e99a
Vanessa Patterson <vpatt1031@gmail.com>
2020-11-29 16:35:53 -0500

    added js form

--- mail.php
+++ mail.php
@@ -3,9 +3,32 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $subject = $_POST['subject'];
-$content="From: $name \n Email: $email \n Message: $message";
+header('Content-Type: application/json');
+if ($name === ''){
+print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
+exit();
+}
+if ($email === ''){
+print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
+exit();
+} else {
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
+exit();
+}
+}
+if ($subject === ''){
+print json_encode(array('message' => 'Subject cannot be empty', 'code' => 0));
+exit();
+}
+if ($message === ''){
+print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
+exit();
+}
+$content="From: $name \nEmail: $email \nMessage: $message";
 $recipient = "vpatt1031@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
-echo "Email sent!";
+print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
+exit();
 ?>
